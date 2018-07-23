9a83f68e129dd20bdc5a1b0b75eb413d195ffda6
LeeHanYeong <dev@lhy.kr>
2018-07-23 16:52:37 +0900

    Mixins기법 사용

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -8,11 +8,12 @@
 # /snippets/api_view/snippets/<pk>/
 from django.urls import path, include
 
-from . import django_view, api_view
+from . import django_view, api_view, mixins
 
 app_name = 'snippets'
 urlpatterns = [
     # config.urls를 참조
     path('django_view/', include(django_view)),
     path('api_view/', include(api_view)),
+    path('mixins/', include(mixins)),
 ]
