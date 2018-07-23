3423b4069b1f01c635ea736234be7d600ed54fd2
LeeHanYeong <dev@lhy.kr>
2018-07-23 15:21:23 +0900

    APIView사용

--- app/snippets/urls/__init__.py
+++ app/snippets/urls/__init__.py
@@ -2,13 +2,17 @@
 # /snippets/snippets/<pk>/
 
 # /snippets/django_view/snippets/
-# /snippets/django_view/snippets/<pk>
+# /snippets/django_view/snippets/<pk>/
+
+# /snippets/api_view/snippets/
+# /snippets/api_view/snippets/<pk>/
 from django.urls import path, include
 
-from . import django_view
+from . import django_view, api_view
 
 app_name = 'snippets'
 urlpatterns = [
     # config.urls를 참조
     path('django_view/', include(django_view)),
+    path('api_view/', include(api_view)),
 ]
